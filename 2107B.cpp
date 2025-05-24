10a3ccaa97c0a204b51aa625432555ddce11fd20
mwarsi278401~ <zulqarnainwarsi@gmail.com>
2025-05-24 23:33:16 +0500

    ...

---
+++ 2107B.cpp
@@ -0,0 +1,83 @@
+/********************* بسم الله الرحمن الرحيم ***********************/
+/********************* ٱلْحَمْدُ لِلَّٰهِ رَبِّ ٱلْعَالَمِينَ ************************/
+/******************* Prophet Muhammad صلى الله عليه وسلم ************/
+/*********************** وَقُل رَّبِّ زِدْنِي عِلْمًا ***************************/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>; //find_by_order() order_of_key()
+
+
+//===========================================================================
+#define int  long long
+#define ld long double
+#define nl cout<<"\n";
+#define pb push_back
+#define F first
+#define S second
+#define deb(x) cout << #x << "=" << x << "\n" 
+#define deb2(x,y) cout << #x << "=" << x << "," << #y << "=" << y <<"\n" 
+int gcd(int a, int b){if(b == 0) {return a;}else {return gcd(b, a % b);}}
+int lcm(int a,int b){return (a*b)/gcd(a,b);}
+//===========================================================================
+
+
+const int mod1 = 1000000007;
+const int mod2 = 998244353;
+void solve();
+
+
+
+signed main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    t=1;
+    cin>>t;
+
+    while(t--){
+        solve();
+        cout<<"\n";
+    }
+
+
+    return 0;
+}
+
+
+void solve(){
+    int n,k;
+    cin>>n>>k;
+    int sum=0;
+    int mx=-1;
+    int mx2=-1;
+    int mn=(1ll<<60);
+    for(int i=0;i<n;i++){
+        int t;
+        cin>>t;
+        sum+=t;
+        if(t>mx){
+            mx2=mx;
+            mx=t;
+        }else if(t>mx2){
+            mx2=t;
+        }
+        mn=min(mn,t);
+    }
+    mx--;
+    mx=max(mx,mx2);
+    if(sum%2==0 || mx-mn>k){
+        cout<<"Jerry";
+        return;
+    }
+    cout<<"Tom";
+    
+}
