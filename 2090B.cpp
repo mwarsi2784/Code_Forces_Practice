b0b176ba0e424e6a33b75611210df932c667b750
mwarsi278401~ <zulqarnainwarsi@gmail.com>
2025-05-27 23:41:16 +0500

    Update 2090B.cpp

--- 2090B.cpp
+++ 2090B.cpp
@@ -85,8 +85,8 @@ void solve(){
     bool f=true;
     for(int i=0;i<n && f;i++){
         for(int j=0;j<m;j++){
-            if(a[i][j] && vis[i][j]){
-                
+            if(a[i][j] && !vis[i][j]){
+
             }
         }
     }
