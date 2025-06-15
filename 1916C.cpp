3eb94f7241dcfaab99b8ff676150ff318bc3c0df
mwarsi278401~ <zulqarnainwarsi@gmail.com>
2025-06-15 23:15:20 +0500

    Update 1916C.cpp

---
+++ 1916C.cpp
@@ -0,0 +1,72 @@
+/********************* بسم الله الرحمن الرحيم ***********************/
+/********************* ٱلْحَمْدُ لِلَّٰهِ رَبِّ ٱلْعَالَمِينَ ************************/
+/******************* Prophet Muhammad صلى الله عليه وسلم ************/
+/*********************** وَقُل رَّبِّ زِدْنِي عِلْمًا ***************************/
+#include<bits/stdc++.h>
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+using namespace __gnu_pbds;
+
+template<typename T>
+using ordered_set = tree<T,null_type,less<T>,rb_tree_tag,tree_order_statistics_node_update>; //find_by_order() order_of_key()
+
+
+//===========================================================================
+#define int  long long
+#define ld long double
+#define nl cout<<"\n";
+#define pb push_back
+#define F first
+#define S second
+#define deb(x) cout << #x << "=" << x << "\n" 
+#define deb2(x,y) cout << #x << "=" << x << "," << #y << "=" << y <<"\n" 
+int gcd(int a, int b){if(b == 0) {return a;}else {return gcd(b, a % b);}}
+int lcm(int a,int b){return (a*b)/gcd(a,b);}
+//===========================================================================
+
+
+const int mod1 = 1000000007;
+const int mod2 = 998244353;
+void solve();
+
+
+
+signed main(){
+
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    t=1;
+    cin>>t;
+
+    while(t--){
+        solve();
+        cout<<"\n";
+    }
+
+
+    return 0;
+}
+
+
+void solve(){
+    int n;
+    cin>>n;
+    int a[n];
+    for(int i=0;i<n;i++) cin>>a[i];
+    int sum=0;
+    cout<<a[0]<<" ";
+    sum=a[0];
+    for(int i=1;i<n;i++){
+        sum+=a[i];
+        if(a[i]&1){
+            cout<<sum-1<<" ";
+        }else{
+            cout<<sum<<" ";
+        }
+    }
+}
