7fae55498b406be3ac76d85dca9fa4a2a9d6023b
mwarsi278401~ <zulqarnainwarsi@gmail.com>
2025-06-22 22:59:22 +0500

    Update 2113B.cpp

--- 2113B.cpp
+++ 2113B.cpp
@@ -56,5 +56,10 @@ signed main(){
 void solve(){
     int w,h,a,b;
     cin>>w>>h>>a>>b;
-    int 
+    int x1,y1,x2,y2;
+    cin>>x1>>y1>>x2>>y2;
+    if(x1+a>x2 && (x2-x1-a)%(a)!=0){
+        cout<<"No";
+        return;
+    }
 }
